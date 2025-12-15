9d38ff8506dad2b544335eb751df84f371b9f961
UltralyticsAssistant <web@ultralytics.com>
2025-12-15 04:20:30 +0000

    Auto-format by https://ultralytics.com/actions

--- 02_PruneDistill/utilPruneDistill.py
+++ 02_PruneDistill/utilPruneDistill.py
@@ -1,10 +1,7 @@
-import cv2
 import sys
 from pathlib import Path
-import os
-import numpy as np
 
-# ----- add the parent folder to environment ----- 
+# ----- add the parent folder to environment -----
 ROOT = Path(__file__).resolve().parents[1]
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
